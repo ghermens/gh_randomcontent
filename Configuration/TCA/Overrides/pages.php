5303f3824a2ba701bfd7b0814542cef6e8de3939
Gregor Hermens <gregor.hermens@a-mazing.de>
2019-09-16 17:43:11 +0200

    apply current naming conventions to files and directories

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,6 +1,6 @@
 <?php
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
     'gh_randomcontent',
-    'Configuration/PageTS/NewContentelementWizard.ts',
+    'Configuration/TsConfig/Page/NewContentelementWizard.typoscript',
     'Add RandomContent to New CE Wizard'
 );
