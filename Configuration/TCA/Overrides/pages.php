bdcf4b82a85e97a740ab2ab9d6f20f8d6fa450ea
Gregor Hermens <gregor.hermens@a-mazing.de>
2019-09-24 16:36:04 +0200

    [TASK] CGL improvements

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,6 +1,6 @@
 <?php
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
     'gh_randomcontent',
-    'Configuration/TsConfig/Page/NewContentelementWizard.typoscript',
+    'Configuration/TsConfig/Page/Mod/Wizards/NewContentElement.tsconfig',
     'Add RandomContent to New CE Wizard'
 );
