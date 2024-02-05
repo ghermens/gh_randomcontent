e0e9bfaaa1416c32a13442ba07f78a7f89af6e65
Gregor Hermens <gregor.hermens@a-mazing.de>
2024-02-05 10:01:38 +0100

    [TASK] Use CE instead of plugin

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,20 +1,38 @@
 <?php
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
     [
-        'label' => 'LLL:EXT:gh_randomcontent/Resources/Private/Language/locallang_db.xlf:tt_content.list_type_pi1',
-        'value' => 'gh_randomcontent_pi1',
+        'label' => 'LLL:EXT:gh_randomcontent/Resources/Private/Language/locallang_db.xlf:tt_content.ghrandomcontent_pi1',
+        'value' => 'ghrandomcontent_pi1',
         'icon' => 'tx-ghrandomcontent-plugin',
         'group' => 'special',
-    ],
-    'list_type',
-    'gh_randomcontent',
+    ]
 );
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['gh_randomcontent_pi1']='pages,layout,select_key,recursive';
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['gh_randomcontent_pi1']='pi_flexform';
+$GLOBALS['TCA']['tt_content']['types']['ghrandomcontent_pi1'] = [
+    'showitem' => '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;;general,
+            --palette--;;headers,
+            pi_flexform,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:appearance,
+            --palette--;;frames,
+            --palette--;;appearanceLinks,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+            --palette--;;language,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;hidden,
+            --palette--;;access,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+            categories,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+            rowDescription,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,',
+];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    'gh_randomcontent_pi1',
-    'FILE:EXT:gh_randomcontent/Configuration/FlexForm/PluginRandomContent.xml'
+    '*',
+    'FILE:EXT:gh_randomcontent/Configuration/FlexForm/PluginRandomContent.xml',
+    'ghrandomcontent_pi1'
 );
