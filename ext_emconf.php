962b393b36299e04d67bee1a66ced50c39618d21
Gregor Hermens <gregor.hermens@a-mazing.de>
2025-11-26 17:14:59 +0100

    [TASK] Declare TYPO3 14 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF['gh_randomcontent'] = [
         [
             'depends' =>
                 [
-                    'typo3' => '12.4.0-13.4.99',
+                    'typo3' => '12.4.0-14.99.99',
                 ],
             'conflicts' =>
                 [],
